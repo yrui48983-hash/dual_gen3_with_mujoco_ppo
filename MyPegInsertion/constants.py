a37ac5d5c8705c505cdf20dbdeee0c0188f3dca4
yrui48983-hash <yrui48983@gmail.com>
2026-05-02 01:11:45 +0800

    constants.py

--- MyPegInsertion/constants.py
+++ MyPegInsertion/constants.py
@@ -1,20 +1,19 @@
 # MyPegInsertion/constants.py
 """Constants for MyPegInsertion environment."""
 
-
 ARM_JOINTS = [
-    "left_arm_joint_1",
-    "left_arm_joint_2",
-    "left_arm_joint_3",
-    "left_arm_joint_4",
-    "left_arm_joint_5",
-    "left_arm_joint_6",
-    "left_arm_joint_7",
-    "right_arm_joint_1",
-    "right_arm_joint_2",
-    "right_arm_joint_3",
-    "right_arm_joint_4",
-    "right_arm_joint_5",
-    "right_arm_joint_6",
-    "right_arm_joint_7",
+    "left_joint_1",
+    "left_joint_2",
+    "left_joint_3",
+    "left_joint_4",
+    "left_joint_5",
+    "left_joint_6",
+    "left_joint_7",
+    "right_joint_1",
+    "right_joint_2",
+    "right_joint_3",
+    "right_joint_4",
+    "right_joint_5",
+    "right_joint_6",
+    "right_joint_7",
 ]
